d7402b8864beac165f974ae791c604de688d465f
paul antezana <contacto@paulantezana.com>
2018-07-08 10:27:52 -0500

    new updates

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,4 +1,8 @@
+// export default {
+//     API_PATH: process.env.API_PATH || "http://localhost:7070",
+//     GRAPHQL_PATH: process.env.GRAPHQL_PATH || "http://localhost:7070/graphql"
+// }
 export default {
-    API_PATH: process.env.API_PATH || "http://localhost:7070",
-    GRAPHQL_PATH: process.env.GRAPHQL_PATH || "http://localhost:7070/graphql"
+    API_PATH: process.env.API_PATH || "https://instituttional.herokuapp.com",
+    GRAPHQL_PATH: process.env.GRAPHQL_PATH || "https://instituttional.herokuapp.com/graphql"
 }
