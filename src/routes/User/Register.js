2c2381e20f5abe57482d57a994f408070cd4cf03
paul antezana <contacto@paulantezana.com>
2018-07-08 10:20:09 -0500

    new updates

--- src/routes/User/Register.js
+++ src/routes/User/Register.js
@@ -10,12 +10,22 @@ const RegisterForm = (props)=>{
     return (
         <div className={styles.form}>
             <Form onSubmit={onSubmit} className={styles.login}>
-                {/* <Form.Item>
+                <Form.Item>
                     {
                         getFieldDecorator('usuario', {
                             rules: [{ required: true, message: 'Por favor ingrese su nombre de usuario!' }],
                         })(
                             <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Nombre de usuario" />
+                        )
+                    }
+                </Form.Item>
+
+                <Form.Item>
+                    {
+                        getFieldDecorator('correo', {
+                            rules: [{ required: true, message: 'Por favor ingrese su contraseña!' }],
+                        })(
+                            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Contraseña"/>
                         )
                     }
                 </Form.Item>
@@ -28,7 +38,18 @@ const RegisterForm = (props)=>{
                             <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Contraseña"/>
                         )
                     }
-                </Form.Item> */}
+                </Form.Item>
+                
+                <Form.Item>
+                    {
+                        getFieldDecorator('clave_confirmar', {
+                            rules: [{ required: true, message: 'Por favor ingrese su contraseña!' }],
+                        })(
+                            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Contraseña"/>
+                        )
+                    }
+                </Form.Item>
+
                 <Form.Item>
                     <Button type="primary" htmlType="submit" className={styles.submit}>Registrarse</Button>
                 </Form.Item>
